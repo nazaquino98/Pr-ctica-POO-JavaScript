10bb420f527b4c286a0758da35049d3a807e30ad
Nazarena Aquino <nazarenaayelen49@gmail.com>
2025-08-07 12:45:50 -0300

    creacion de la Clase: CuentaBancaria con get y set

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // 1. Función constructora: CuentaBancaria
 
-function CuentaBancaria(titular, saldoInicial){
+function CuentaBancaria(titular, saldoInicial) {
   this.titular = titular;
   this.saldoInicial = saldoInicial;
 }
 
 const cuentaBancariaPrototype = {
-  depositar: function(monto) {
+  depositar: function (monto) {
     this.saldoInicial += monto;
     console.log(`Se depositaron $${monto}. Nuevo saldo: $${this.saldoInicial}`);
   },
 
-  extraer: function(monto){
-    if (monto <= this.saldoInicial){
+  extraer: function (monto) {
+    if (monto <= this.saldoInicial) {
       this.saldoInicial -= monto;
       console.log(`Se extrajeron $${monto}. Nuevo saldo: $${this.saldoInicial}`);
     } else {
@@ -20,7 +20,7 @@ const cuentaBancariaPrototype = {
     }
   },
 
-  consultarSaldo: function(){
+  consultarSaldo: function () {
     console.log(`Saldo actual: $${this.saldoInicial}`);
   }
 };
@@ -32,7 +32,52 @@ miCuenta.consultarSaldo();
 miCuenta.depositar(3000);
 miCuenta.extraer(2000);
 
-// 2. Clase: CuentaBancaria con get y set
+//2. Clase: CuentaBancaria con get y set
+
+class CuentaBancaria2 {
+  #saldo; 
+
+  constructor(titular, saldoInicial) {
+    this.titular = titular;
+    this.saldo = saldoInicial;
+  }
+
+  
+  get saldo() {
+    return this.#saldo;
+  }
+
+  
+  set saldo(nuevoSaldo) {
+    if (nuevoSaldo >= 0) {
+      this.#saldo = nuevoSaldo;
+    } else {
+      console.log("El saldo no puede ser negativo.");
+    }
+  }
+
+  depositar(monto) {
+    this.saldo = this.saldo + monto; 
+    console.log(`Se depositaron $${monto}.`);
+  }
+
+  extraer(monto) {
+    if (monto <= this.saldo) {
+      this.saldo = this.saldo - monto;
+      console.log(`Se extrajeron $${monto}.`);
+    } else {
+      console.log("Fondos insuficientes");
+    }
+  }
+
+  consultarSaldo() {
+    console.log(`Saldo actual: $${this.saldo}`);
+  }
+}
+
+const miCuenta2 = new CuentaBancaria2("Nazarena Aquino", 10000)
+
+miCuenta2.consultarSaldo();
 
 
 
@@ -44,11 +89,3 @@ miCuenta.extraer(2000);
 
 
 
-// const myObject = {
-//   city: "Madrid",
-//   greet() {
-//     console.log(`Greetings from ${this.city}`);
-//   },
-// };
-
-// myObject.greet(); // Greetings from Madrid
